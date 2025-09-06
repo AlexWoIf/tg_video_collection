fcb845c48f7ddff2f473897a0559214a8945569f
AlexWoIf <alexey.barannikov@gmail.com>
2025-09-06 11:22:14 +0300

    One more validation

--- config.py
+++ config.py
@@ -12,12 +12,15 @@ class Config():
         db_name = os.getenv('DB_NAME', '')
         db_user = os.getenv('DB_USER', '')
         db_password = os.getenv('DB_PASSWORD', '')
-        if not db_name or not db_user or not db_password:
-            raise ValueError('Some DB params are not set')
         self.db_url = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}' # noqa: E501
 
-        self.tg_bot_token = os.getenv('TG_BOT_TOKEN', '')
+        tg_bot_token = os.getenv('TG_BOT_TOKEN', '')
+        self.tg_bot_token = tg_bot_token
         tg_base_url = os.getenv('TG_BASE_URL', 'https://api.telegram.org/bot')
-        self.tg_api_url = f'{tg_base_url}{self.tg_bot_token}'
+        self.tg_api_url = f'{tg_base_url}{tg_bot_token}'
 
         self.storage_chat_id = os.getenv('STORAGE_CHAT_ID', '')
+
+        # TODO: use pydantic instead
+        if not db_name or not db_user or not db_password or not tg_bot_token:
+            raise ValueError('Some params does not set')

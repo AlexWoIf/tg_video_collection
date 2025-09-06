3ebb679e39c0283e976a6bdf1367380d02aec39c
AlexWoIf <alexey.barannikov@gmail.com>
2025-09-06 11:19:40 +0300

    Partial config validation

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-import logging
 
 from dotenv import load_dotenv
 
@@ -10,11 +9,15 @@ class Config():
 
         db_host = os.getenv('DB_HOST', 'localhost')
         db_port = os.getenv('DB_PORT', '3306')
-        db_name = os.getenv('DB_NAME', 'your_database_name')
-        db_user = os.getenv('DB_USER', 'your_username')
-        db_password = os.getenv('DB_PASSWORD', 'your_password')
+        db_name = os.getenv('DB_NAME', '')
+        db_user = os.getenv('DB_USER', '')
+        db_password = os.getenv('DB_PASSWORD', '')
+        if not db_name or not db_user or not db_password:
+            raise ValueError('Some DB params are not set')
         self.db_url = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}' # noqa: E501
 
         self.tg_bot_token = os.getenv('TG_BOT_TOKEN', '')
-        self.tg_api_url = os.getenv('TG_API_URL', 'https://api.telegram.org/bot') # noqa: E501
-        self.storage_channel_id = os.getenv('STORAGE_CHANNEL_ID', '')
+        tg_base_url = os.getenv('TG_BASE_URL', 'https://api.telegram.org/bot')
+        self.tg_api_url = f'{tg_base_url}{self.tg_bot_token}'
+
+        self.storage_chat_id = os.getenv('STORAGE_CHAT_ID', '')
